1281692bc6206fa4f075b31b8bdb16a2914ce34f
mashal <333mashal@gmail.com>
2020-04-03 12:43:13 +0530

    configure for Glide and UI changes

--- app/src/main/java/com/example/campusapp/backend/DataRef.kt
+++ app/src/main/java/com/example/campusapp/backend/DataRef.kt
@@ -2,11 +2,13 @@ package com.example.campusapp.backend
 
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 // this class is indented to do backend queries. Currently used only as reference for const labels for queries
 object DataRef {
     val db = Firebase.firestore
-
+    val storage = Firebase.storage
 //    private var forumsDocSnap: List<DocumentSnapshot> = listOf()
 //    private var projectsDocSnap: List<DocumentSnapshot> = listOf()
     const val FORUMS_COLLECTION = "forums"
--- app/src/main/java/com/example/campusapp/ui/main/forum/ForumFragment.kt
+++ app/src/main/java/com/example/campusapp/ui/main/forum/ForumFragment.kt
@@ -2,24 +2,27 @@ package com.example.campusapp.ui.main.forum
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.example.campusapp.R
 import com.example.campusapp.backend.DataRef
 import com.example.campusapp.backend.DataRef.db
+import com.example.campusapp.backend.DataRef.storage
+import com.example.campusapp.tool.GlideApp
 import com.google.firebase.firestore.DocumentSnapshot
 import kotlinx.android.synthetic.main.forum_detail.*
+import kotlin.math.roundToInt
 
 
 class ForumFragment : Fragment() {
     private var mDoc: List<DocumentSnapshot> = listOf()
-    private val TAG = "Forum"
+    private val TAG = "ForumFragment"
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: MessageAdapter
@@ -36,31 +39,38 @@ class ForumFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val safeArgs: ForumFragmentArgs by navArgs()
         val forumId = safeArgs.forumId
-        forum_id_tv.text = forumId
         chat_progress_bar.visibility = View.VISIBLE
-        // Data for forum basic info
+
+        updateUI(view, forumId)
+
+    }
+
+    private fun updateUI(view:View ,forumId: String) {
+        // retrieving header info
+        var forumName = "Forum"
         db.document(DataRef.FORUMS_COLLECTION+"/"+forumId).get()
             .addOnSuccessListener { document ->
                 if (document != null) {
-                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                    forum_title.text = document.getString("title")
+//                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                    forumName = document.getString("title")!!
+                    viewAdapter.updateData(forumName)
+                    recyclerView.adapter!!.notifyItemChanged(0)
+
                 } else {
                     Log.d(TAG, "No such document")
                 }
-                forum_progress.visibility = View.GONE
             }
             .addOnFailureListener { exception ->
                 Log.d(TAG, "get() failed with ", exception)
-                forum_progress.visibility = View.GONE
             }
-
         viewManager = LinearLayoutManager(context)
-        viewAdapter = MessageAdapter(mDoc)
+        viewAdapter = MessageAdapter(mDoc, forumId, forumName)
 
         recyclerView = view.findViewById<RecyclerView>(R.id.chat_recycler).apply {
             layoutManager = viewManager
             adapter = viewAdapter
         }
+
         // retrieving messages
         db.collection(DataRef.FORUMS_COLLECTION + "/" + forumId + "/" + DataRef.FORUMS_MESSAGES)
             .addSnapshotListener { value, e ->
@@ -71,9 +81,9 @@ class ForumFragment : Fragment() {
                 }
                 if (value != null && !value.isEmpty) {
                     mDoc = value.documents
-//                    for (doc in mDoc) { Log.v(TAG, "${doc.id} -> ${doc.get("text")}") }
                     viewAdapter.updateData(mDoc)
                     recyclerView.adapter!!.notifyDataSetChanged()
+//                    for (doc in mDoc) { Log.v(TAG, "${doc.id} -> ${doc.get("text")}") }
                 }
             }
     }
